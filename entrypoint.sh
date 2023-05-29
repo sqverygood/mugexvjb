ae14a629f7ba098a106c1f2665a54ff35abf236c
sqverygood <74126711+sqverygood@users.noreply.github.com>
2023-05-29 09:37:53 +0000

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,272 +1,105 @@
 #!/usr/bin/env bash
 
+# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/131054d3-2c9e-4596-a865-b52306ef29d2-vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/131054d3-2c9e-4596-a865-b52306ef29d2-vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/131054d3-2c9e-4596-a865-b52306ef29d2-trojan'}
-SS_WSPATH=${SS_WSPATH:-'/131054d3-2c9e-4596-a865-b52306ef29d2-shadowsocks'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
+SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
+sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
-generate_config() {
-  cat > config.json << EOF
-{
-    "log":{
-        "access":"/dev/null",
-        "error":"/dev/null",
-        "loglevel":"none"
-    },
-    "inbounds":[
-        {
-            "port":8080,
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "flow":"xtls-rprx-vision"
-                    }
-                ],
-                "decryption":"none",
-                "fallbacks":[
-                    {
-                        "dest":3001
-                    },
-                    {
-                        "path":"${VLESS_WSPATH}",
-                        "dest":3002
-                    },
-                    {
-                        "path":"${VMESS_WSPATH}",
-                        "dest":3003
-                    },
-                    {
-                        "path":"${TROJAN_WSPATH}",
-                        "dest":3004
-                    },
-                    {
-                        "path":"${SS_WSPATH}",
-                        "dest":3005
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"tcp"
-            }
-        },
-        {
-            "port":3001,
-            "listen":"127.0.0.1",
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}"
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none"
-            }
-        },
-        {
-            "port":3002,
-            "listen":"127.0.0.1",
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "level":0
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none",
-                "wsSettings":{
-                    "path":"${VLESS_WSPATH}"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3003,
-            "listen":"127.0.0.1",
-            "protocol":"vmess",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "alterId":0
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"ws",
-                "wsSettings":{
-                    "path":"${VMESS_WSPATH}"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3004,
-            "listen":"127.0.0.1",
-            "protocol":"trojan",
-            "settings":{
-                "clients":[
-                    {
-                        "password":"${UUID}"
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none",
-                "wsSettings":{
-                    "path":"${TROJAN_WSPATH}"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3005,
-            "listen":"127.0.0.1",
-            "protocol":"shadowsocks",
-            "settings":{
-                "clients":[
-                    {
-                        "method":"chacha20-ietf-poly1305",
-                        "password":"${UUID}"
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "wsSettings":{
-                    "path":"${SS_WSPATH}"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls"
-                ],
-                "metadataOnly":false
-            }
+# 设置 nginx 伪装站
+rm -rf /usr/share/nginx/*
+wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
+rm -f /usr/share/nginx/mikutap.zip
+
+# 伪装 xray 执行文件
+RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+mv xray ${RELEASE_RANDOMNESS}
+wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+cat config.json | base64 > config
+rm -f config.json
+
+# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+
+# 启用 Argo，并输出节点日志
+cloudflared tunnel --url http://localhost:80 --no-autoupdate > argo.log 2>&1 &
+sleep 5 && argo_url=$(cat argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
+
+vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"Argo_xray_vmess\",\"add\":\"$argo_url\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$argo_url\",\"path\":\"$VMESS_WSPATH?ed=2048\",\"tls\":\"tls\"}" | base64 -w 0)
+vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$argo_url":443?encryption=none&security=tls&type=ws&host="$argo_url"&path="$VLESS_WSPATH"?ed=2048#Argo_xray_vless"
+trlink=$(echo -e '\x74\x72\x6f\x6a\x61\x6e')"://"$UUID"@"$argo_url":443?security=tls&type=ws&host="$argo_url"&path="$TROJAN_WSPATH"?ed2049#Argo_xray_trojan"
+
+qrencode -o /usr/share/nginx/html/M$UUID.png $vmlink
+qrencode -o /usr/share/nginx/html/L$UUID.png $vllink
+qrencode -o /usr/share/nginx/html/T$UUID.png $trlink
+
+cat > /usr/share/nginx/html/$UUID.html<<-EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Argo-xray-paas</title>
+    <style type="text/css">
+        body {
+            font-family: Geneva, Arial, Helvetica, san-serif;
         }
-    ],
-    "outbounds":[
-        {
-            "protocol":"freedom"
-        },
-        {
-            "tag": "WARP",
-            "protocol": "wireguard",
-            "settings": {
-                "secretKey": "GAl2z55U2UzNU5FG+LW3kowK+BA/WGMi1dWYwx20pWk=",
-                "address": [
-                    "172.16.0.2/32",
-                    "2606:4700:110:8f0a:fcdb:db2f:3b3:4d49/128"
-                ],
-                "peers": [
-                    {
-                        "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                        "endpoint": "engage.cloudflareclient.com:2408"
-                    }
-                ]
-            }
+
+        div {
+            margin: 0 auto;
+            text-align: left;
+            white-space: pre-wrap;
+            word-break: break-all;
+            max-width: 80%;
+            margin-bottom: 10px;
         }
-    ],
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "domain": [
-                    "domain:openai.com",
-                    "domain:ai.com"
-                ],
-                "outboundTag": "WARP"
-            }
-        ]
-    },
-    "dns":{
-        "servers":[
-            "https+local://8.8.8.8/dns-query"
-        ]
-    }
-}
+    </style>
+</head>
+<body bgcolor="#FFFFFF" text="#000000">
+    <div>
+        <font color="#009900"><b>VMESS协议链接：</b></font>
+    </div>
+    <div>$vmlink</div>
+    <div>
+        <font color="#009900"><b>VMESS协议二维码：</b></font>
+    </div>
+    <div><img src="/M$UUID.png"></div>
+    <div>
+        <font color="#009900"><b>VLESS协议链接：</b></font>
+    </div>
+    <div>$vllink</div>
+    <div>
+        <font color="#009900"><b>VLESS协议二维码：</b></font>
+    </div>
+    <div><img src="/L$UUID.png"></div>
+    <div>
+        <font color="#009900"><b>TROJAN协议链接：</b></font>
+    </div>
+    <div>$trlink</div>
+    <div>
+        <font color="#009900"><b>TROJAN协议二维码：</b></font>
+    </div>
+    <div><img src="/T$UUID.png"></div>
+    <div>
+        <font color="#009900"><b>SS协议明文：</b></font>
+    </div>
+    <div>服务器地址：$argo_url</div>
+    <div>端口：443</div>
+    <div>密码：$UUID</div>
+    <div>加密方式：chacha20-ietf-poly1305</div>
+    <div>传输协议：ws</div>
+    <div>host：$argo_url</div>
+    <div>path路径：$SS_WSPATH?ed=2048</div>
+    <div>TLS：开启</div>
+</body>
+</html>
 EOF
-}
 
-generate_nezha() {
-  cat > nezha.sh << EOF
-#!/usr/bin/env bash
-
-# 哪吒的三个参数
-NEZHA_SERVER=${NEZHA_SERVER}
-NEZHA_PORT=${NEZHA_PORT}
-NEZHA_KEY=${NEZHA_KEY}
-
-# 检测是否已运行
-check_run() {
-    [[ \$(pidof nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
-}
-
-# 三个变量不全则不安装哪吒客户端
-check_variable() {
-    [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
-}
-
-# 下载最新版本 Nezha Agent
-download_agent() {
-    if [ ! -e nezha-agent ]; then
-        URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
-        wget -t 2 -T 10 -N \${URL}
-        unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
-    fi
-}
-
-# 运行客户端
-run() {
-    [ -e nezha-agent ] && chmod +x nezha-agent && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY}
-}
-
-check_run
-check_variable
-download_agent
-run
-wait
-EOF
-}
-
-generate_config
-generate_nezha
-[ -e nezha.sh ] && bash nezha.sh 2>&1 &
-wait
+nginx
+base64 -d config > config.json
+./${RELEASE_RANDOMNESS} -config=config.json
